68caafc73eda7fc7d4988a6f1c255db2f050fb3c
Roon Le <ronbanafato@yahoo.com>
2020-02-03 23:53:19 -0800

    Develop UserControllerTest, need to make group and gift

---
+++ SecretSanta/test/SecretSanta.Data.Tests/SampleData.cs
@@ -0,0 +1,17 @@
+﻿
+namespace SecretSanta.Data.Tests
+{
+    static public class SampleData
+    {
+        public const string Inigo = "Inigo";
+        public const string Montoya = "Montoya";
+        
+
+        public const string Princess = "Princess";
+        public const string Buttercup = "Buttercup";
+        
+
+        static public User CreateInigoMontoya() => new User(Inigo, Montoya);
+        static public User CreatePrincessButtercup() => new User(Princess, Buttercup);
+    }
+}
