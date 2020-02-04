367411acc967216fb8c0fa2563a495007bb0004b
Roon Le <ronbanafato@yahoo.com>
2020-02-03 21:26:10 -0800

    Setting up dependency injection with register services

--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -2,6 +2,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SecretSanta.Business;
+using SecretSanta.Business.Services;
+using SecretSanta.Data;
+using Microsoft.Data.Sqlite;
+using AutoMapper;
+
 
 namespace SecretSanta.Api
 {
@@ -10,11 +18,28 @@ namespace SecretSanta.Api
     public class Startup
     #pragma warning restore CA1052 // Static holder types should be Static or NotInheritable
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        //public void ConfigureServices(IServiceCollection services)
-        //{
-        //}
+        public static void ConfigureServices(IServiceCollection services)
+        {
+#pragma warning disable CA2000 // connection is used further out
+            var sqliteConnection = new SqliteConnection("DataSource=:memory:");
+#pragma warning restore CA2000 // connection is used further out
+
+            sqliteConnection.Open();
+
+            services.AddDbContext<ApplicationDbContext>(options => options.EnableSensitiveDataLogging()
+            .UseSqlite(sqliteConnection));
+
+            services.AddScoped<IGiftService, GiftService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IGroupService, GroupService>();
+
+            System.Type profileType = typeof(AutomapperConfigurationProfile);
+            System.Reflection.Assembly assembly = profileType.Assembly;
+            services.AddAutoMapper(new[] { assembly });
+            services.AddMvc(opts => opts.EnableEndpointRouting = false);
+            services.AddSwaggerDocument();
+
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -26,13 +51,12 @@ namespace SecretSanta.Api
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                _ = endpoints.MapGet("/", async context =>
-                  {
-                      await context.Response.WriteAsync("Hello from API!");
-                  });
-            });
+            app.UseOpenApi();
+            
+            app.UseSwaggerUi3();
+
+            app.UseMvc();
         }
     }
 }
+
