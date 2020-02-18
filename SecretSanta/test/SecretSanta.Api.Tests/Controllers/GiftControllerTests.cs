3df54dcddfb92ca5436c5b922a2b457ed85b7641
Roon Le <ronbanafato@yahoo.com>
2020-02-18 15:50:24 -0800

    Create integration test for giftController

---
+++ SecretSanta/test/SecretSanta.Api.Tests/Controllers/GiftControllerTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using SecretSanta.Data;
+using SecretSanta.Data.Tests;
+using System.Text.Json;
+using System.Text;
+
+namespace SecretSanta.Api.Tests.Controllers
+{
+    [TestClass]
+    public class GiftControllerTests
+    {
+#pragma warning disable CS8618 // justification its get intiailized in TestSetup.
+        private SecretSantaWebApplicationFactory Factory { get; set; }
+#pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+
+        [TestInitialize]
+
+        public void TestSetup()
+        {
+            Factory = new SecretSantaWebApplicationFactory();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Factory.Dispose();
+        }
+
+        [TestMethod]
+
+        public async Task Get_ReturnsGifts()
+        {
+            //Arrange
+            using ApplicationDbContext context = Factory.GetDbContext();
+            context.Database.EnsureCreated();
+            Data.Gift im = SampleData.CreateBatmanGift();
+            context.Gifts.Add(im);
+            context.SaveChanges();
+            HttpClient client = Factory.CreateClient();
+
+            //Act
+            //justification using a string is okay in this project other then uri object, by design
+#pragma warning disable CA2234 // Pass system uri objects instead of strings
+            HttpResponseMessage response = await client.GetAsync("api/Gift");
+#pragma warning restore CA2234 // Pass system uri objects instead of strings
+
+            //Assert
+            response.EnsureSuccessStatusCode();
+            string jsonData = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            
+            };
+            Business.Dto.Gift[] gifts = JsonSerializer.Deserialize<Business.Dto.Gift[]>(jsonData, options);
+
+            Assert.AreEqual(1, gifts.Length);
+            Assert.AreEqual(im.Id, gifts[0].Id);
+            Assert.AreEqual(im.Title, gifts[0].Title);
+            Assert.AreEqual(im.Url, gifts[0].Url);
+            Assert.AreEqual(im.UserId, gifts[0].UserId);
+            
+        
+
+        }
+    }
+}
---
+++ SecretSanta/test/SecretSanta.Data.Tests/SampleData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Data.Tests
+{
+    static public class SampleData
+    {
+        public const string Inigo = "Inigo";
+        public const string Montoya = "Montoya";
+
+
+        public const string Princess = "Princess";
+        public const string Buttercup = "Buttercup";
+
+        public const string SantaGroup = "SantaGroup";
+
+        public const string BatmanGift = "Batman";
+        public const string BatmanUrl = "www.Batman.com";
+        public const string BatmanDescription = "Plushy";
+
+
+        static public User CreateInigoMontoya() => new User(Inigo, Montoya);
+        static public User CreatePrincessButtercup() => new User(Princess, Buttercup);
+
+        static public Group CreateSantaGroup() => new Group(SantaGroup);
+
+        static public Gift CreateBatmanGift() => new Gift(BatmanGift, BatmanUrl, BatmanDescription, CreateInigoMontoya());
+    }
+}
