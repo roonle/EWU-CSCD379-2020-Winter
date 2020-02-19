2e1cc268dcef8fca7805ad3a5951f4275d6d6f25
Roon Le <ronbanafato@yahoo.com>
2020-02-19 00:10:28 -0800

    View and web controller

---
+++ SecretSanta/src/SecretSanta.Web/Controllers/GiftController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SecretSanta.Web.Api;
+
+namespace SecretSanta.Web.Controllers
+{
+    public class GiftController : Controller
+    {
+        public GiftController(IHttpClientFactory clientFactory)
+        {
+            if (clientFactory is null)
+            {
+                throw new System.ArgumentNullException(nameof(clientFactory));
+            }
+
+            ClientFactory = clientFactory;
+        }
+
+        public IHttpClientFactory ClientFactory { get; }
+
+       
+        public async Task<ActionResult> Index()
+        {
+            HttpClient httpClient = ClientFactory.CreateClient("SecretSantaApi");
+
+            var client = new GiftClient(httpClient);
+            ICollection<Gift> gifts = await client.GetAllAsync();
+            return View(gifts);
+        }
+    }
+}
