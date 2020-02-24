a0c1cb57ec381d6faa98f274d1b72b6241aaf2ec
Roon Le <ronbanafato@yahoo.com>
2020-02-23 22:13:23 -0800

    add layout file

--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -20,8 +20,7 @@ namespace SecretSanta.Api
         {
             Configuration = configuration;
         }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
         public  void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -38,7 +37,7 @@ namespace SecretSanta.Api
             services.AddAutoMapper(new[] { typeof(AutomapperConfigurationProfile).Assembly });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+       
         public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
